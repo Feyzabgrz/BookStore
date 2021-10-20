dcf1ee0937acd6e56c2f8dec304cae236bc642d5
Feyzabgrz <feyzabgrz@gmail.comm>
2021-10-20 14:13:10 +0300

    AutoMapper

---
+++ WebApi/Common/MappingProfile.cs
@@ -0,0 +1,25 @@
+
+using AutoMapper;
+using static WebApi.BookOperations.CreateBook.CreateBookCommand;
+using static WebApi.BookOperations.GetBookDetail.GetBookDetailQuery;
+using static WebApi.BookOperations.GetBooks.GetBooksQuery;
+
+namespace WebApi.Common
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile()
+        {
+            //İlk parametre source,2. parametre target yani 1. 2. ye dönüştürülsün
+            CreateMap<CreateBookModel,Book>(); //CreateBookModel objesi Book objesine maplenebilir olsun
+
+            //Burda ihtiyaç duyaağımız bazı satırları map etmesini isteyeceğiz
+            CreateMap<Book,BookDetailViewModel>().ForMember(dest=>dest.Genre, opt=>opt.MapFrom(src=>((GenreEnum)src.GenreId).ToString()));
+
+            CreateMap<Book,BooksViewModel>().ForMember(dest=>dest.Genre, opt=>opt.MapFrom(src=>((GenreEnum)src.GenreId).ToString()));
+
+
+        }
+
+    }
+}
